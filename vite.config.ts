e8a2f96d3c56dfd8f10853ebc36dae1f8c2f262d
Dexter Yang <dexter.yy@gmail.com>
2025-04-01 12:00:28 +0800

    update WebSpatial

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import react from "@vitejs/plugin-react";
 import WebSpatial from "@webspatial/vite-plugin";
 import { createHtmlPlugin } from "vite-plugin-html";
 
-const basename =
-  process.env.XR_ENV === "avp"
-    ? `/webspatial/${process.env.XR_ENV}`
-    : undefined;
-
-console.log("basename: ", basename);
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -23,9 +16,4 @@ export default defineConfig({
       },
     }),
   ],
-  // temporary
-  // base: basename,
-  define: {
-    __XR_ENV_BASE__: basename ? JSON.stringify(basename) : undefined,
-  },
 });
